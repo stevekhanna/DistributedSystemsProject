98c082e11eaa85f079978cab567c08b9b37e2c45
Steve Khanna <stvkhanna@gmail.com>
2021-03-29 21:57:06 -0600

    Testers updated

--- CopyIntoTest.sh
+++ CopyIntoTest.sh
@@ -4,4 +4,5 @@
 cp out/artifacts/DistributedSystemsProject_jar/DistributedSystemsProject.jar testDir/tmp/
 mv testDir/tmp/DistributedSystemsProject.jar testDir/tmp/Client.jar
 #Copy Server jar into testDir
+mv out/artifacts/ServerIter3/DistributedSystemsProject.jar out/artifacts/ServerIter3/ServerIter3.jar
 cp out/artifacts/ServerIter3/ServerIter3.jar testDir/tmp/
---
+++ LinusServer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+jdk-14.0.2/bin/java -jar testDir/tmp/ServerIter3.jar
+
